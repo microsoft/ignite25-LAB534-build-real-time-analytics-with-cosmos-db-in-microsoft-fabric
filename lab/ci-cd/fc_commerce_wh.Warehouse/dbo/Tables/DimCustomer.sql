a6011ff900c08a25cb70c8b14be3e19f1aeed2d2
Jcardif <29174946+Jcardif@users.noreply.github.com>
2025-10-23 11:41:40 +0300

    create warehouse on fabric and load it with data from the csv files.

--- lab/ci-cd/fc_commerce_wh.Warehouse/dbo/Tables/DimCustomer.sql
+++
@@ -1,20 +0,0 @@
-CREATE TABLE [dbo].[DimCustomer] (
-
-	[CustomerKey] int NOT NULL, 
-	[CustomerId] varchar(64) NOT NULL, 
-	[CustomerName] varchar(128) NULL, 
-	[Email] varchar(256) NULL, 
-	[PreferredAirport] varchar(32) NULL, 
-	[FavoriteDrink] varchar(128) NULL, 
-	[LoyaltyTier] varchar(32) NULL, 
-	[RegistrationDate] date NULL, 
-	[IsActive] bit NULL, 
-	[CreatedAt] datetime2(3) NULL, 
-	[UpdatedAt] datetime2(3) NULL
-);
-
-
-GO
-ALTER TABLE [dbo].[DimCustomer] ADD CONSTRAINT PK_DimCustomer primary key NONCLUSTERED ([CustomerKey]);
-GO
-ALTER TABLE [dbo].[DimCustomer] ADD CONSTRAINT UQ_DimCustomer_CustomerId unique NONCLUSTERED ([CustomerId]);
--- lab/ci-cd/fc_commerce_wh.Warehouse/dbo/Tables/FactSales.sql
+++
@@ -1,35 +0,0 @@
-CREATE TABLE [dbo].[FactSales] (
-
-	[SalesKey] bigint NOT NULL, 
-	[TransactionId] varchar(64) NOT NULL, 
-	[DateKey] int NOT NULL, 
-	[TimeKey] int NOT NULL, 
-	[CustomerKey] int NOT NULL, 
-	[ShopKey] int NOT NULL, 
-	[MenuItemKey] int NOT NULL, 
-	[Quantity] int NOT NULL, 
-	[UnitPrice] decimal(10,2) NOT NULL, 
-	[TotalAmount] decimal(10,2) NOT NULL, 
-	[LoyaltyPointsEarned] int NULL, 
-	[LoyaltyPointsRedeemed] int NULL, 
-	[PaymentMethod] varchar(32) NULL, 
-	[Size] varchar(32) NULL, 
-	[SourceSystem] varchar(50) NULL, 
-	[CreatedAt] datetime2(3) NULL
-);
-
-
-GO
-ALTER TABLE [dbo].[FactSales] ADD CONSTRAINT PK_FactSales primary key NONCLUSTERED ([SalesKey]);
-GO
-ALTER TABLE [dbo].[FactSales] ADD CONSTRAINT UQ_FactSales_TransactionId unique NONCLUSTERED ([TransactionId]);
-GO
-ALTER TABLE [dbo].[FactSales] ADD CONSTRAINT FK_FactSales_Customer FOREIGN KEY ([CustomerKey]) REFERENCES [dbo].[DimCustomer]([CustomerKey]);
-GO
-ALTER TABLE [dbo].[FactSales] ADD CONSTRAINT FK_FactSales_Date FOREIGN KEY ([DateKey]) REFERENCES [dbo].[DimDate]([DateKey]);
-GO
-ALTER TABLE [dbo].[FactSales] ADD CONSTRAINT FK_FactSales_MenuItem FOREIGN KEY ([MenuItemKey]) REFERENCES [dbo].[DimMenuItem]([MenuItemKey]);
-GO
-ALTER TABLE [dbo].[FactSales] ADD CONSTRAINT FK_FactSales_Shop FOREIGN KEY ([ShopKey]) REFERENCES [dbo].[DimShop]([ShopKey]);
-GO
-ALTER TABLE [dbo].[FactSales] ADD CONSTRAINT FK_FactSales_Time FOREIGN KEY ([TimeKey]) REFERENCES [dbo].[DimTime]([TimeKey]);
